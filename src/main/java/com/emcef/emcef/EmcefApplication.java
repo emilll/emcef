0a70a430c36cc3961ee5ef3a50cd3f0f8c0d16e8
BOSSOU Emmanuel <emmanuelromeobossou@gmail.com>
2021-03-30 17:40:51 +0200

    Signed-off-by: BOSSOU Emmanuel <emmanuelromeobossou@gmail.com>

---
+++ src/main/java/com/emcef/emcef/EmcefApplication.java
@@ -0,0 +1,13 @@
+package com.emcef.emcef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmcefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmcefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emcef/emcef/EmcefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emcef.emcef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmcefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
