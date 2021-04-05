6cb64ceeed04a92ceba74c98b786f6e5b61ba7e7
BOSSOU Emmanuel <emmanuelromeobossou@gmail.com>
2021-04-05 21:35:28 +0100

    OK

--- src/main/java/com/emcef/model/Contribuable.java
+++ src/main/java/com/emcef/model/Contribuable.java
@@ -1,19 +1,11 @@
 package com.emcef.model;
 
 
-<<<<<<< HEAD
-=======
-// <<<<<<< HEAD:src/main/java/com/emcef/model/Contribuable/Contribuables.java
-
-// =======
-// >>>>>>> a1ec80c78514114371ea4e5799274f9baf9d8145:src/main/java/com/emcef/model/Contribuable.java
->>>>>>> origin/production
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -21,206 +13,200 @@ import javax.persistence.Table;
 public class Contribuable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @OneToOne
-    CentresFiscaux centresFiscaux;
+    private int _id;
 
     @Column(name = "date_heure")
-    private String date_heure;
+    private String _date_heure;
     
     @Column(name = "ifu")
-    private String ifu;
+    private String _ifu;
     
     @Column(name = "rccm")
-    private String rccm;
+    private String _rccm;
     
     @Column(name = "nom")
-    private String nom;
+    private String _nom;
     
     @Column(name = "adresse")
-    private String adresse;
+    private String _adresse;
     
     @Column(name = "adresse2")
-    private String adresse2;
+    private String _adresse2;
     
     @Column(name = "adresse3")
-    private String adresse3;
+    private String _adresse3;
     
     @Column(name = "adresse4")
-    private String adresse4;
+    private String _adresse4;
     
     @Column(name = "ville")
-    private String ville;
+    private String _ville;
     
     @Column(name = "zip")
-    private String zip;
+    private String _zip;
     
     @Column(name = "description_location")
-    private String description_location;
+    private String _description_location;
     
     @Column(name = "telephone")
-    private String telephone;
+    private String _telephone;
     
     @Column(name = "contact_personnel")
-    private String contact_personnel;
+    private String _contact_personnel;
     
     @Column(name = "email")
-    private String email;
+    private String _email;
     
     @Column(name = "date_enregistrement")
-    private String date_enregistrement;
+    private String _date_enregistrement;
     
     @Column(name = "id_activite")
-    private Integer id_activite;
+    private Integer _id_activite;
     
     @Column(name = "commentaire")
-    private String commentaire;
+    private String _commentaire;
     
      public int getId() {
-        return id;
+        return _id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId(int _id) {
+        this._id = _id;
     }
 
     public String getDate_heure() {
-        return date_heure;
+        return _date_heure;
     }
 
-    public void setDate_heure(String date_heure) {
-        this.date_heure = date_heure;
+    public void setDate_heure(String _date_heure) {
+        this._date_heure = _date_heure;
     }
 
     public String getIfu() {
-        return ifu;
+        return _ifu;
     }
 
-    public void setIfu(String ifu) {
-        this.ifu = ifu;
+    public void setIfu(String _ifu) {
+        this._ifu = _ifu;
     }
 
     public String getRccm() {
-        return rccm;
+        return _rccm;
     }
 
-    public void setRccm(String rccm) {
-        this.rccm = rccm;
+    public void setRccm(String _rccm) {
+        this._rccm = _rccm;
     }
 
     public String getNom() {
-        return nom;
+        return _nom;
     }
 
-    public void setNom(String nom) {
-        this.nom = nom;
+    public void setNom(String _nom) {
+        this._nom = _nom;
     }
 
     public String getAdresse() {
-        return adresse;
+        return _adresse;
     }
 
-    public void setAdresse(String adresse) {
-        this.adresse = adresse;
+    public void setAdresse(String _adresse) {
+        this._adresse = _adresse;
     }
 
     public String getAdresse2() {
-        return adresse2;
+        return _adresse2;
     }
 
-    public void setAdresse2(String adresse2) {
-        this.adresse2 = adresse2;
+    public void setAdresse2(String _adresse2) {
+        this._adresse2 = _adresse2;
     }
 
     public String getAdresse3() {
-        return adresse3;
+        return _adresse3;
     }
 
-    public void setAdresse3(String adresse3) {
-        this.adresse3 = adresse3;
+    public void setAdresse3(String _adresse3) {
+        this._adresse3 = _adresse3;
     }
 
     public String getAdresse4() {
-        return adresse4;
+        return _adresse4;
     }
 
-    public void setAdresse4(String adresse4) {
-        this.adresse4 = adresse4;
+    public void setAdresse4(String _adresse4) {
+        this._adresse4 = _adresse4;
     }
 
     public String getVille() {
-        return ville;
+        return _ville;
     }
 
-    public void setVille(String ville) {
-        this.ville = ville;
+    public void setVille(String _ville) {
+        this._ville = _ville;
     }
 
     public String getZip() {
-        return zip;
+        return _zip;
     }
 
-    public void setZip(String zip) {
-        this.zip = zip;
+    public void setZip(String _zip) {
+        this._zip = _zip;
     }
 
     public String getDescription_location() {
-        return description_location;
+        return _description_location;
     }
 
-    public void setDescription_location(String description_location) {
-        this.description_location = description_location;
+    public void setDescription_location(String _description_location) {
+        this._description_location = _description_location;
     }
 
     public String getTelephone() {
-        return telephone;
+        return _telephone;
     }
 
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
+    public void setTelephone(String _telephone) {
+        this._telephone = _telephone;
     }
 
     public String getContact_personnel() {
-        return contact_personnel;
+        return _contact_personnel;
     }
 
-    public void setContact_personnel(String contact_personnel) {
-        this.contact_personnel = contact_personnel;
+    public void setContact_personnel(String _contact_personnel) {
+        this._contact_personnel = _contact_personnel;
     }
 
     public String getEmail() {
-        return email;
+        return _email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEmail(String _email) {
+        this._email = _email;
     }
 
     public String getDate_enregistrement() {
-        return date_enregistrement;
+        return _date_enregistrement;
     }
 
-    public void setDate_enregistrement(String date_enregistrement) {
-        this.date_enregistrement = date_enregistrement;
+    public void setDate_enregistrement(String _date_enregistrement) {
+        this._date_enregistrement = _date_enregistrement;
     }
 
     public Integer getId_activite() {
-        return id_activite;
+        return _id_activite;
     }
 
-    public void setId_activite(Integer id_activite) {
-        this.id_activite = id_activite;
+    public void setId_activite(Integer _id_activite) {
+        this._id_activite = _id_activite;
     }
 
     public String getCommentaire() {
-        return commentaire;
+        return _commentaire;
     }
 
-    public void setCommentaire(String commentaire) {
-        this.commentaire = commentaire;
-    }
-
-    public Contribuable() {
+    public void setCommentaire(String _commentaire) {
+        this._commentaire = _commentaire;
     }
 }
